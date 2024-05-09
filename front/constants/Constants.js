94751a73d022f4cad834a972cdf7061d4ec3e1c4
Call-Me-G-Now <125204742+Call-Me-G-Now@users.noreply.github.com>
2024-05-09 21:43:57 +0200

    Prettyfy some stuff (#28)
    * update version tag on settings page
    
    * add repo url to about page
    
    ---------
    
    Co-authored-by: call-me-g-now <call-me-g-now@localhost>

--- front/constants/Constants.js
+++ front/constants/Constants.js
@@ -1,5 +1,5 @@
 
 const FIREFLY_URL = "https://money.cioraneanu.com"
-export default {
-    FIREFLY_URL
-}
+export default { FIREFLY_URL }
+
+export const REPO_URL = "https://github.com/cioraneanu/firefly-pico/tags"
--- front/pages/settings/about.vue
+++ front/pages/settings/about.vue
@@ -20,7 +20,8 @@
     <van-cell-group inset>
       <div class="van-cell-group-title mb-0">Credits</div>
       <div class="text-muted text-size-12 p-20 pt-10">
-        <div> - Developed by Mihai Cioraneanu</div>
+        <div> - Developed by Mihai Cioraneanu - <a :href="REPO_URL">Source on GitHub</a></div>
+        <br>
         <div> - Using <a href="https://laravel.com/" target="_blank">Laravel</a> and <a href="https://nuxt.com/" target="_blank">Nuxt</a></div>
         <div> - Consuming the <a href="https://www.firefly-iii.org" target="_blank">Firefly III</a> REST API</div>
         <div> - With a UI theme based on <a href="https://vant-ui.github.io" target="_blank">Vant</a></div>
@@ -49,6 +50,7 @@ import { useAppStore } from '~/stores/appStore'
 import { useDataStore } from '~/stores/dataStore'
 import { useToolbar } from '~/composables/useToolbar'
 import RouteConstants from '~/constants/RouteConstants'
+import { REPO_URL } from '~/constants/Constants'
 
 const appStore = useAppStore()
 const dataStore = useDataStore()
