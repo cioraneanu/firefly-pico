cfad44173375b01bf5c5af27bb76279165e932b1
mihaicioraneanu <cioraneanumihai@gmail.com>
2024-05-09 12:21:27 +0300

    Added eslint + prettier

---
+++ front/eslint.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  // rules: {
+  //   'no-console': 'off', // allow console.log in TypeScript files
+  // },
+})
