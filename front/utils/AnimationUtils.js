8a83191f4c73beaad14b20c81f61f972d8ff8376
Cioraneanu Mihai <cioraneanumihai@gmail.com>
2024-06-12 16:01:13 +0300

    * added dashboard animation

--- front/utils/AnimationUtils.js
+++ front/utils/AnimationUtils.js
@@ -12,3 +12,30 @@ export function animateSwipeList(list) {
     })
   })
 }
+
+export async function animateDashboard() {
+  await nextTick()
+
+  anime({
+    targets: `.van-cell-group`,
+    translateX: [-50, 0],
+    opacity: [0, 1],
+    delay: anime.stagger(200, { start: 0 }),
+  })
+
+  // for (let i = 1; i <= 6; i++) {
+  //   let selector = `.app-form .van-cell-group:nth-child(${i + 1})`
+  //   // let translate = i % 2 === 0 ? [-100, 0] : [100, 0]
+  //   let translate = [-100, 0]
+  //   let delay = 50 * i
+  //   anime({
+  //     targets: selector,
+  //     translateX: translate,
+  //     delay: delay,
+  //     duration: 500,
+  //     // opacity: [0, 1],
+  //     // delay: anime.stagger(200, { start: 0 }),
+  //   })
+  // }
+
+}
