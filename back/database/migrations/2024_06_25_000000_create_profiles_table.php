aecd91f938cb6729a9989f5b57792b3552448d27
Cioraneanu Mihai <cioraneanumihai@gmail.com>
2024-06-26 14:29:08 +0300

    * added profiles.id

--- back/database/migrations/2024_06_25_000000_create_profiles_table.php
+++ back/database/migrations/2024_06_25_000000_create_profiles_table.php
@@ -4,16 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
 
     public function up(): void
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->string("auth_token_hash")->primary();
-            $table->timestamps();
-
+            $table->id();
+            $table->string("auth_token_hash")->unique();
             $table->jsonb('settings')->default('{}');
+            $table->timestamps();
         });
     }
 
