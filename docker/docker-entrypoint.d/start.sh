03d2ad1e30d80e4e772428f375fbaf819efcc613
Cioraneanu Mihai <cioraneanumihai@gmail.com>
2024-06-29 16:17:17 +0300

    Update start.sh to clear cache

--- docker/docker-entrypoint.d/start.sh
+++ docker/docker-entrypoint.d/start.sh
@@ -3,4 +3,5 @@
 php /var/www/html/artisan migrate --isolated --force
 php /var/www/html/artisan config:clear
 php /var/www/html/artisan config:cache
+php /var/www/html/artisan cache:clear
 supervisord -c /etc/supervisord.conf
