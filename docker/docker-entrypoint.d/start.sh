9169a15a59f9ab6365c3322f372dce4050b9e9be
Cioraneanu Mihai <cioraneanumihai@gmail.com>
2024-09-27 21:59:05 +0300

    attempt to make the app wait for DB

--- docker/docker-entrypoint.d/start.sh
+++ docker/docker-entrypoint.d/start.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+while true; do
+    if nc -z -w 5 $DB_HOST $DB_PORT; then
+        echo "Successfully connected to DB"
+        break
+    else
+        echo "Failed to connect to DB. Retrying in 10 seconds..."
+        sleep 10
+    fi
+done
+
 php /var/www/html/artisan migrate --isolated --force
 php /var/www/html/artisan config:clear
 php /var/www/html/artisan config:cache
