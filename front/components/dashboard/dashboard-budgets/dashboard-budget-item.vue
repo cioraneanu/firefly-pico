68953ed9ac0cd136d6bf733329048a0adf869149
avee87 <6134677+avee87@users.noreply.github.com>
2024-10-01 22:39:30 +0100

    Use budget limit amount on dashboard

--- front/components/dashboard/dashboard-budgets/dashboard-budget-item.vue
+++ front/components/dashboard/dashboard-budgets/dashboard-budget-item.vue
@@ -6,7 +6,7 @@
     <template #text>
       <div class="display-flex flex-column align-items-center">
         <div class="font-600 text-size-12 text-center">{{ displayName }}</div>
-        <div class="font-500 text-size-10 text-center">{{ getFormattedValue(budgetLimitSpent) }} / {{ getFormattedValue(budgetAmount) }} {{ budgetCurrencySymbol }}</div>
+        <div class="font-500 text-size-10 text-center">{{ getFormattedValue(budgetLimitSpent) }} / {{ getFormattedValue(budgetLimitAmount) }} {{ budgetCurrencySymbol }}</div>
         <div class="font-500 text-size-10 text-center text-muted">{{ budgetLimitInterval }}</div>
       </div>
     </template>
@@ -28,8 +28,8 @@ const props = defineProps({
 
 const budgetLimit = computed(() => Budget.getLimit(props.value))
 const displayName = computed(() => get(props.value, 'attributes.name', ' - '))
-const budgetAmount = computed(() => get(props.value, 'attributes.amount', ' - '))
 const budgetLimitSpent = computed(() => Math.abs(get(budgetLimit.value, `attributes.spent`, 0)))
+const budgetLimitAmount = computed(() => get(budgetLimit.value, 'attributes.amount') ?? 0)
 const budgetLimitInterval = computed(() => BudgetLimit.getLimitInterval(budgetLimit.value))
 const budgetCurrencySymbol = computed(() => Budget.getCurrencySymbol(props.value))
 
