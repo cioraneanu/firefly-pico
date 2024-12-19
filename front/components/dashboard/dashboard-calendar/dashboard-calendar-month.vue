2b4f4fce0c4d2b8be38aea713c6829e301ab2728
Cioraneanu Mihai <cioraneanumihai@gmail.com>
2024-12-19 14:31:28 +0200

    - first draft to render 2 months

--- front/components/dashboard/dashboard-calendar/dashboard-calendar-month.vue
+++ front/components/dashboard/dashboard-calendar/dashboard-calendar-month.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="month">
     <div class="month-header flex-1 display-flex-column pb-2">
-      <div class="month-title text-center p-2 font-700"><span class="text-primary mr-5">{{ monthName }}</span> {{ yearName }}</div>
-      <div class="display-flex font-400 text-size-12">
+      <div v-if="showMonthName" class="month-title text-center p-2 font-700">
+        <span class="text-primary mr-5">{{ monthName }}</span> {{ yearName }}
+      </div>
+      <div v-if="showDaysName" class="display-flex font-400 text-size-12">
         <div v-for="dayName in dayNames" class="text-center flex-1">{{ dayName }}</div>
       </div>
     </div>
@@ -22,22 +24,28 @@ import { capitalize, computed } from 'vue'
 import { addDays, eachDayOfInterval, endOfMonth, endOfWeek, format, getDay, getDaysInMonth, setDate, startOfMonth, startOfWeek } from 'date-fns'
 
 const props = defineProps({
-  month: {
-    type: Date,
-    default: () => startOfMonth(new Date()),
+  start: {},
+  end: {},
+  showMonthName: {
+    default: true,
+  },
+  showDaysName: {
+    default: true,
   },
 })
+
+const month = computed(() => startOfMonth(props.start))
 
 const dayNames = Array.from({ length: 7 }, (_, i) => {
   const date = addDays(startOfWeek(new Date()), i)
   return format(date, 'EEEEEE')
 })
-const monthName = computed(() => capitalize(props.month.toLocaleString('en-EN', { month: 'long' })))
-const yearName = computed(() => props.month.getFullYear())
+const monthName = computed(() => capitalize(props.start.toLocaleString('en-EN', { month: 'long' })))
+const yearName = computed(() => props.start.getFullYear())
 
 const calendar = computed(() => {
-  const start = startOfWeek(startOfMonth(props.month))
-  const end = endOfWeek(endOfMonth(props.month))
+  const start = startOfWeek(props.start)
+  const end = endOfWeek(props.end)
   const days = eachDayOfInterval({ start, end })
   return days.reduce((weeks, day, i) => {
     if (i % 7 === 0) weeks.push([])
--- front/components/dashboard/dashboard-calendar/dashboard-calendar.vue
+++ front/components/dashboard/dashboard-calendar/dashboard-calendar.vue
@@ -1,10 +1,30 @@
 <template>
-  <van-cell-group inset >
-    <dashboard-calendar-month />
+  <van-cell-group inset>
+    <dashboard-calendar-month
+        v-for="(month, index) in monthsList"
+        :start="month.start"
+        :end="month.end"
+        :show-days-name="index === 0"
+        :show-month-name="index === 0"
+    />
   </van-cell-group>
 </template>
 
 <script setup>
-import TablerIconConstants from "~/constants/TablerIconConstants";
-import Transaction from "~/models/Transaction";
+import { eachMonthOfInterval, min, max, startOfMonth, endOfMonth } from 'date-fns'
+
+const dataStore = useDataStore()
+
+const monthsList = computed(() => {
+  const months = eachMonthOfInterval({
+    start: dataStore.dashboardDateStart,
+    end: dataStore.dashboardDateEnd,
+  })
+
+  return months.map((date) => ({
+    month: startOfMonth(date),
+    start: max([dataStore.dashboardDateStart, startOfMonth(date)]),
+    end: min([dataStore.dashboardDateEnd, endOfMonth(date)]),
+  }))
+})
 </script>
