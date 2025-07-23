9ea5f1c66541a568cc485246bd00048ba77cdcf3
Márcio Dias <khigashi.oang@gmail.com>
2025-07-23 11:23:23 -0300

    feat(i18n): add Brazilian Portuguese (pt-BR) translation
    - add pt-BR translation file
    - enable pt-BR locale for date-fns
    - add SVG icon for Brazilian flag

--- front/plugins/plugin-vant.js
+++ front/plugins/plugin-vant.js
@@ -7,6 +7,7 @@ import enUS from 'vant/es/locale/lang/en-US'
 import ro from 'vant/es/locale/lang/ro-RO'
 import zhCN from 'vant/es/locale/lang/zh-CN'
 import italian from 'vant/es/locale/lang/it-IT'
+import ptBR from 'vant/es/locale/lang/pt-BR'
 
 import { languageCode } from '~/i18n/index.js'
 
@@ -19,6 +20,7 @@ export default defineNuxtPlugin((nuxtApp) => {
         [languageCode.romanian]: ro,
         [languageCode.chinese]: zhCN,
         [languageCode.italian]: italian,
+        [languageCode.brazilian_portuguese]: ptBR,
       }
       let locale = localeDictionary[newValue] || enUS
       Locale.use(newValue, locale)
