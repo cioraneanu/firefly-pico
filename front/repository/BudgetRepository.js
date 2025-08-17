f839229671e0f8369f5d8a045760a2ca34daa9c0
Cioraneanu Mihai <cioraneanumihai@gmail.com>
2025-08-17 13:40:13 +0300

    - provide the new required start/end date for budget-limits endpoint

--- front/repository/BudgetRepository.js
+++ front/repository/BudgetRepository.js
@@ -1,6 +1,7 @@
 import BaseRepository from '~/repository/BaseRepository'
 import axios from 'axios'
 import { get } from 'lodash'
+import { endOfMonth, startOfMonth } from 'date-fns'
 
 export default class BudgetRepository extends BaseRepository {
   constructor() {
@@ -9,7 +10,12 @@ export default class BudgetRepository extends BaseRepository {
 
   async getBudgetLimits() {
     const appStore = useAppStore()
-    let response = await axios.get(`${appStore.picoBackendURL}/api/budget-limits`)
+    let start = startOfMonth(new Date())
+    let end = endOfMonth(new Date())
+    let urlParams = `start=${DateUtils.dateToString(start)}&end=${DateUtils.dateToString(end)}`
+    // let urlParams = `start=2025-08-01&end=2025-08-30`
+
+    let response = await axios.get(`${appStore.picoBackendURL}/api/budget-limits?${urlParams}`)
     return get(response, 'data', {})
   }
 }
