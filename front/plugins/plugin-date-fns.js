95f4db6ad275d96e28596074823b3dbde9501a4d
Daniil Mileev <39964648+milden6@users.noreply.github.com>
2026-01-16 15:45:50 +0300

    Add Russian (ru-RU) localization (#250)
    - Add ru-RU.json translation file
    - Add Russian flag SVG icon
    - Wire up locale following existing pattern

--- front/plugins/plugin-date-fns.js
+++ front/plugins/plugin-date-fns.js
@@ -1,5 +1,5 @@
 import { setDefaultOptions } from 'date-fns'
-import { ro, enUS, zhCN, it, ptBR, de, fr} from 'date-fns/locale'
+import { ro, enUS, zhCN, it, ptBR, de, fr, ru} from 'date-fns/locale'
 import { languageCode } from '~/i18n/index.js'
 
 export default defineNuxtPlugin((nuxtApp) => {
@@ -23,6 +23,7 @@ export default defineNuxtPlugin((nuxtApp) => {
         [languageCode.german_german]: de,
         [languageCode.swiss_german]: de,
         [languageCode.french]: fr,
+        [languageCode.russian]: ru,
       }
       let dateFnsLocale = dateFnsLocaleDictionary[newValue] || enUS
 
