595c24fa018911e3644b7b553fa939539e982305
Cioraneanu Mihai <cioraneanumihai@gmail.com>
2026-01-28 13:42:58 +0200

    - make external database/data folder writable for www-data user

--- docker/docker-entrypoint.d/start.sh
+++ docker/docker-entrypoint.d/start.sh
@@ -32,4 +32,8 @@ php /var/www/html/artisan migrate --isolated --force
 php /var/www/html/artisan config:clear
 php /var/www/html/artisan config:cache
 php /var/www/html/artisan cache:clear
+
+chown -R www-data:www-data /var/www/html/database/data
+chmod -R 664 /var/www/html/database/data
+
 supervisord -c /etc/supervisord.conf
