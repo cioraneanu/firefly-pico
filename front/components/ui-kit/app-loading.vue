816f8de6ab43f6859a886ba5b1a4acd089fc7aa8
cioraneanu <cioraneanumihai@gmail.com>
2026-06-05 17:09:26 +0300

    - work

--- front/components/ui-kit/app-loading.vue
+++ front/components/ui-kit/app-loading.vue
@@ -1,15 +1,12 @@
 <template>
   <transition :name="transitionName">
-    <div v-if="showLoading" class="app-loading-background">
+    <div v-if="loadingStore.isLoading" class="app-loading-background">
       <div class="app-loading flex-column flex-center">
         <icon-rotate :size="30" :stroke="1.4" class="animate-rotate-infinite" />
         <div class="text-size-16">{{ loadingStore.loadingMessage }}</div>
         
         <div v-if="loadingStore.activeRequests.length > 0" class="text-size-12 text-muted mt-2">
           {{ loadingStore.activeRequests.length }} {{ $t('requests_remaining') }}
-        </div>
-        <div v-else>
-          WTF
         </div>
         
         <van-button 
@@ -30,38 +27,12 @@ import { IconRotate } from '@tabler/icons-vue'
 import { useLoadingStore } from '~/stores/loadingStore'
 
 const profileStore = useProfileStore()
-const loadingStore = useLoadingStore()
 const transitionName = computed(() => profileStore.showAnimations ? 'fade' : '')
 
-const showLoading = ref(loadingStore.isLoading)
+const loadingStore = useLoadingStore()
 const showCancelButton = ref(false)
-let timeout = null
-let hideTimeout = null
 
-watch(() => loadingStore.isLoading, (isLoading) => {
-  if (isLoading) {
-    if (hideTimeout) clearTimeout(hideTimeout)
-    showLoading.value = true
-    
-    showCancelButton.value = false
-    if (timeout) clearTimeout(timeout)
-    timeout = setTimeout(() => {
-      showCancelButton.value = true
-    }, 2000)
-  } else {
-    if (hideTimeout) clearTimeout(hideTimeout)
-    hideTimeout = setTimeout(() => {
-      showLoading.value = false
-      showCancelButton.value = false
-      if (timeout) clearTimeout(timeout)
-    }, 300)
-  }
-}, { immediate: true })
 
-onUnmounted(() => {
-  if (timeout) clearTimeout(timeout)
-  if (hideTimeout) clearTimeout(hideTimeout)
-})
 </script>
 
 <style></style>
