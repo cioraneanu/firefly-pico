689870754629b0b1f180a0aa3fa35f1a0db542d6
cioraneanu <cioraneanumihai@gmail.com>
2026-06-28 21:41:54 +0300

    - work

---
+++ back/database/migrations/2026_06_28_000000_create_assistant_rambles_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assistant_rambles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('text');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assistant_rambles');
+    }
+};
