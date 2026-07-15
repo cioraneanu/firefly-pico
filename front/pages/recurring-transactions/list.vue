7c56eb5ea42367dda6440a1b998e5ca5ee07c0d8
Cioraneanu Mihai <cioraneanumihai@gmail.com>
2026-07-15 15:57:47 +0300

    - work

--- front/pages/recurring-transactions/list.vue
+++ front/pages/recurring-transactions/list.vue
@@ -10,7 +10,22 @@
 
     <van-pull-refresh v-model="isRefreshing" @refresh="onRefresh">
       <van-list class="p-1" :finished="isFinished" @load="onLoadMore">
-        <app-list-search v-if="isSearchVisible && list.length > 0" v-model="search" />
+        <app-list-search v-if="isSearchVisible && list.length > 0" v-model="search">
+          <template #right>
+            <van-popover v-model:show="showFilterPopover" placement="bottom-end">
+              <div class="display-flex flex-column gap-1 p-10">
+                <div class="text-size-12 font-weight-600 text-muted">{{ $t('transaction_type') }}</div>
+                <app-tabs v-model="filterTransactionType" :items="transactionTypeTabs" />
+              </div>
+
+              <template #reference>
+                <button type="button" class="app-button-icon">
+                  <app-icon :icon="TablerIconConstants.settings" :size="18" />
+                </button>
+              </template>
+            </van-popover>
+          </template>
+        </app-list-search>
         <recurring-transaction-list-item v-for="item in filteredList" :key="item.id" :value="item" @on-edit="onEdit" @on-delete="onDelete" />
       </van-list>
     </van-pull-refresh>
@@ -25,7 +40,10 @@ import { useToolbar } from '~/composables/useToolbar'
 import AppListSearch from '~/components/ui-kit/theme/app-list-search.vue'
 import { animateSwipeList } from '~/utils/AnimationUtils.js'
 import RecurringTransaction from '~/models/RecurringTransaction.js'
+import Transaction from '~/models/Transaction.js'
+import TablerIconConstants from '~/constants/TablerIconConstants.js'
 
+const { t } = useI18n()
 const recurringTransactionStore = useRecurringTransactionStore()
 
 const onEvent = (event, payload) => {
@@ -37,11 +55,24 @@ const onEvent = (event, payload) => {
 const search = ref('')
 const isSearchVisible = ref(true)
 
+const showFilterPopover = ref(false)
+const filterTransactionType = ref('all')
+const transactionTypeTabs = computed(() => [
+  { label: t('transaction.type.all'), value: 'all' },
+  { label: t('transaction.type.expense'), value: Transaction.types.expense.code },
+  { label: t('transaction.type.income'), value: Transaction.types.income.code },
+  { label: t('transaction.type.transfer'), value: Transaction.types.transfer.code },
+])
+
 const filteredList = computed(() => {
-  if (search.value.length === 0) {
-    return list.value
+  let result = list.value
+  if (filterTransactionType.value !== 'all') {
+    result = result.filter((item) => RecurringTransaction.getType(item)?.code === filterTransactionType.value)
   }
-  return list.value.filter((item) => {
+  if (search.value.length === 0) {
+    return result
+  }
+  return result.filter((item) => {
     return RecurringTransaction.getDisplayName(item).toUpperCase().indexOf(search.value.toUpperCase()) !== -1
   })
 })
@@ -82,7 +113,6 @@ const onLoadMore = () => {
 
 // -----
 
-const { t } = useI18n()
 const toolbar = useToolbar()
 toolbar.init({
   title: t('recurring_transactions'),
